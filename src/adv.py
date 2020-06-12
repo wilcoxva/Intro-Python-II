539644cb0af06529ddaf10cfdc9af4d03d50e430
Virginia Wilcox <wilcoxva@gmail.com>
2020-06-11 20:37:20 -0400

    added list print to adv

--- src/adv.py
+++ src/adv.py
@@ -48,6 +48,7 @@ while True:
     print(player.current_room.name)
     # * Prints the current description (the textwrap module might be useful here).
     print(player.current_room.description)
+    print(player.current_room.list)
     # * Waits for user input and decides what to do.
     input_var = input("Type a direction.")
     #
