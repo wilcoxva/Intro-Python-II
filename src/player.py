63045583842e4448a1c7ec95c72261a0f797ca0f
Virginia Wilcox <wilcoxva@gmail.com>
2020-06-11 20:38:32 -0400

    added list to player.py

--- src/player.py
+++ src/player.py
@@ -1,6 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, list):
         self.name = name
         self.current_room = current_room
+        self.list = list
